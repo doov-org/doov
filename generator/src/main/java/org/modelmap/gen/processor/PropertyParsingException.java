29b5520692266ab6943df16d56b2b97950383012
gdu <gdigugli@gmail.com>
2015-10-07 17:29:38 +0200

    fixed a few warnings

--- generator/src/main/java/org/modelmap/gen/processor/PropertyParsingException.java
+++ generator/src/main/java/org/modelmap/gen/processor/PropertyParsingException.java
@@ -1,5 +1,6 @@
 package org.modelmap.gen.processor;
 
+@SuppressWarnings("serial")
 public class PropertyParsingException extends RuntimeException {
 
     public PropertyParsingException(String message) {
