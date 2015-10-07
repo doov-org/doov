c98e944348d48e7438ca2cc57f06bf45faa10f4b
gdu <gdigugli@gmail.com>
2015-10-07 14:15:26 +0200

    use Arrays.stream instead of Arrays.asList

--- sample/test/src/main/java/org/modelmap/sample/model/CloneBenchmark.java
+++ sample/test/src/main/java/org/modelmap/sample/model/CloneBenchmark.java
@@ -1,46 +1,26 @@
-/*
- * Copyright (c) 2014, Oracle America, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- *  * Neither the name of Oracle nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
+
 
 package org.modelmap.sample.model;
 
-import static java.util.Arrays.asList;
 import static org.modelmap.sample.model.FavoriteWebsite.webSite;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import org.modelmap.core.FieldModel;
 import org.modelmap.core.FieldModels;
 import org.modelmap.sample.field.SampleFieldIdInfo;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.Throughput)
@@ -90,22 +70,19 @@ public class CloneBenchmark {
     public FieldModel clone_field_model() {
         FieldModel fieldModel = new SampleModelWrapper(source);
         FieldModel clone = new SampleModelWrapper();
-
-        asList(fieldModel.getFieldInfos()).forEach(e -> clone.set(e.id(), fieldModel.get(e.id())));
+        Arrays.stream(fieldModel.getFieldInfos()).forEach(e -> clone.set(e.id(), fieldModel.get(e.id())));
         return clone;
     }
 
     @Benchmark
     public FieldModel clone_stream_sequential() {
-        return FieldModels.stream(new SampleModelWrapper(source), false)
-                        .filter(e -> e.getValue() != null)
+        return FieldModels.stream(new SampleModelWrapper(source), false).filter(e -> e.getValue() != null)
                         .collect(FieldModels.toFieldModel(SampleFieldIdInfo.values()));
     }
 
     @Benchmark
     public FieldModel clone_stream_parallel() {
-        return FieldModels.stream(new SampleModelWrapper(source), true)
-                        .filter(e -> e.getValue() != null)
+        return FieldModels.stream(new SampleModelWrapper(source), true).filter(e -> e.getValue() != null)
                         .collect(FieldModels.toFieldModel(SampleFieldIdInfo.values()));
     }
 }
