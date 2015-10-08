7796f8e036610303b672a25459caddb592bcee15
jba <jba@lesfurets.com>
2015-10-08 17:46:35 +0200

    single instance collector

--- core/src/main/java/org/modelmap/core/FieldModels.java
+++ core/src/main/java/org/modelmap/core/FieldModels.java
@@ -1,7 +1,9 @@
 package org.modelmap.core;
 
+import java.util.EnumSet;
 import java.util.Map.Entry;
-import java.util.function.Supplier;
+import java.util.Set;
+import java.util.function.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -30,5 +32,47 @@ public class FieldModels {
         return Collectors.collectingAndThen(
                         Collectors.toMap(Entry::getKey, Entry::getValue),
                         m -> new BaseFieldModel(m, fieldInfos));
+    }
+
+    /**
+     * Returns a {@code Collector} that accumulates the input elements into a unique {@code FieldModel}.
+     */
+    public static Collector<Entry<FieldId, Object>, ?, FieldModel> toFieldModel(FieldModel model) {
+        return new FieldModelCollector(model);
+    }
+
+    private static final class FieldModelCollector
+                    implements Collector<Entry<FieldId, Object>, FieldModel, FieldModel> {
+
+        private final FieldModel model;
+
+        FieldModelCollector(FieldModel model) {
+            this.model = model;
+        }
+
+        @Override
+        public Supplier<FieldModel> supplier() {
+            return () -> model;
+        }
+
+        @Override
+        public BiConsumer<FieldModel, Entry<FieldId, Object>> accumulator() {
+            return (model, entry) -> model.set(entry.getKey(), entry.getValue());
+        }
+
+        @Override
+        public BinaryOperator<FieldModel> combiner() {
+            return (m1, m2) -> m1;
+        }
+
+        @Override
+        public Function<FieldModel, FieldModel> finisher() {
+            return (m) -> model;
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return EnumSet.allOf(Characteristics.class);
+        }
     }
 }
