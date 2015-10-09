e09b39526ac4fc22b09f1195f7e64edd78e1f047
gdu <gdigugli@gmail.com>
2015-10-09 18:27:40 +0200

    Arrays.stream as static import

--- sample/generated/src/main/java/org/modelmap/sample2/model/Sample2Models.java
+++ sample/generated/src/main/java/org/modelmap/sample2/model/Sample2Models.java
@@ -1,6 +1,7 @@
 package org.modelmap.sample2.model;
 
 import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
 import static org.modelmap.sample2.field.Sample2Tag.EMAIL;
 import static org.modelmap.sample2.field.Sample2Tag.ID;
 import static org.modelmap.sample2.field.Sample2Tag.LOGIN;
@@ -15,23 +16,19 @@ public class Sample2Models {
     public static Sample2Model sample() {
         final Sample2ModelWrapper wrapper = new Sample2ModelWrapper();
         final AtomicLong i = new AtomicLong(0);
-        Arrays.stream(wrapper.getFieldInfos()).map(FieldInfo::id)
-                        .filter(id -> asList(id.tags()).contains(EMAIL))
+        stream(wrapper.getFieldInfos()).map(FieldInfo::id).filter(id -> asList(id.tags()).contains(EMAIL))
                         .forEach(id -> wrapper.set(id, "account" + i.incrementAndGet() + "@some.name"));
 
         i.set(0);
-        Arrays.stream(wrapper.getFieldInfos()).map(FieldInfo::id)
-                        .filter(id -> asList(id.tags()).contains(ID))
+        stream(wrapper.getFieldInfos()).map(FieldInfo::id).filter(id -> asList(id.tags()).contains(ID))
                         .forEach(id -> wrapper.set(id, i.incrementAndGet()));
 
         i.set(0);
-        Arrays.stream(wrapper.getFieldInfos()).map(FieldInfo::id)
-                        .filter(id -> asList(id.tags()).contains(PASSWORD))
+        stream(wrapper.getFieldInfos()).map(FieldInfo::id).filter(id -> asList(id.tags()).contains(PASSWORD))
                         .forEach(id -> wrapper.set(id, "password_" + i.incrementAndGet()));
 
         i.set(0);
-        Arrays.stream(wrapper.getFieldInfos()).map(FieldInfo::id)
-                        .filter(id -> asList(id.tags()).contains(LOGIN))
+        stream(wrapper.getFieldInfos()).map(FieldInfo::id).filter(id -> asList(id.tags()).contains(LOGIN))
                         .forEach(id -> wrapper.set(id, "login_" + i.incrementAndGet()));
 
         return wrapper.getModel();
