78620f8e8342fb39a5c3d334c3c12e3a48ded7a3
gdu <gdigugli@gmail.com>
2016-05-31 15:21:47 +0200

    no more ValueDifference use org.apache.commons.lang3.tuple.Triple

--- sample/test/src/test/java/org/modelmap/example/ModelManipulationTest.java
+++ sample/test/src/test/java/org/modelmap/example/ModelManipulationTest.java
@@ -4,14 +4,22 @@
 package org.modelmap.example;
 
 import java.util.Collections;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.tuple.Triple;
 import org.junit.Test;
+import org.modelmap.core.FieldId;
 import org.modelmap.core.FieldModel;
 import org.modelmap.sample.field.SampleFieldId;
-import org.modelmap.sample.model.*;
+import org.modelmap.sample.model.SampleModel;
+import org.modelmap.sample.model.SampleModelWrapper;
+import org.modelmap.sample.model.SampleModels;
 
 public class ModelManipulationTest {
 
@@ -37,28 +45,43 @@ public class ModelManipulationTest {
 
     @Test
     public void diff() {
-        FieldModel sampleV1 = SampleModels.wrapper();
-        FieldModel sampleV2 = SampleModels.wrapper();
+        FieldModel sample_1 = SampleModels.wrapper();
+        FieldModel sample_2 = SampleModels.wrapper();
 
-        sampleV1.set(SampleFieldId.FAVORITE_SITE_NAME_3, null);
-        sampleV1.set(SampleFieldId.FAVORITE_SITE_URL_3, null);
-
-        sampleV2.set(SampleFieldId.FAVORITE_SITE_NAME_1, "LesFurets.com");
-        sampleV2.set(SampleFieldId.FAVORITE_SITE_URL_1, "www.lesfurets.com");
-        sampleV2.set(SampleFieldId.EMAILS_PREFERENCES, Collections.emptyList());
+        sample_1.set(SampleFieldId.FAVORITE_SITE_NAME_3, null);
+        sample_1.set(SampleFieldId.FAVORITE_SITE_URL_3, null);
+        sample_2.set(SampleFieldId.FAVORITE_SITE_NAME_1, "LesFurets.com");
+        sample_2.set(SampleFieldId.FAVORITE_SITE_URL_1, "www.lesfurets.com");
+        sample_2.set(SampleFieldId.EMAILS_PREFERENCES, Collections.emptyList());
 
         /* stream all key-values pair from both models */
-        Stream.concat(sampleV1.parallelStream().map(ValueDifference::left),
-                        sampleV2.parallelStream().map(ValueDifference::right))
+        Stream.concat(sample_1.stream().map(buildRight), sample_2.stream().map(buildLeft))
 
                         /* merging key-value pair in a map */
-                        .collect(Collectors.toMap(ValueDifference::getKey, Function.identity(), ValueDifference::merge))
+                        .collect(Collectors.toMap(Triple::getMiddle, Function.identity(), merge))
 
                         /* filter to keep only key with 2 differents values */
-                        .values().stream().filter(diff -> !diff.isEquals())
+                        .values().stream().filter(isNotSame)
 
                         /* print keys with differents values */
-                        .forEachOrdered(System.out::println);
+                        .forEach(System.out::println);
     }
 
+    static Function<Entry<FieldId, Object>, Triple<Object, FieldId, Object>> buildLeft = (entry) -> {
+        return Triple.of(entry.getValue(), entry.getKey(), null);
+    };
+
+    static Function<Entry<FieldId, Object>, Triple<Object, FieldId, Object>> buildRight = (entry) -> {
+        return Triple.of(null, entry.getKey(), entry.getValue());
+    };
+
+    static Predicate<Triple<Object, FieldId, Object>> isNotSame = (triple) -> {
+        return !Objects.equals(triple.getLeft(), triple.getRight());
+    };
+
+    static BinaryOperator<Triple<Object, FieldId, Object>> merge = (t1, t2) -> {
+        Object left = t1.getLeft() != null ? t1.getLeft() : t2.getLeft();
+        Object right = t2.getRight() != null ? t2.getRight() : t1.getRight();
+        return Triple.of(left, t1.getMiddle(), right);
+    };
 }
