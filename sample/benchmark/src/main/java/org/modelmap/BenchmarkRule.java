a838719f8da4551962dee9f573bc61f74f3b8eb1
Alexandre DuBreuil <adu@lesfurets.com>
2017-10-16 18:18:34 +0200

    Correct benchmark compilation error

--- sample/benchmark/src/main/java/org/modelmap/BenchmarkRule.java
+++ sample/benchmark/src/main/java/org/modelmap/BenchmarkRule.java
@@ -1,12 +1,30 @@
 package org.modelmap;
 
-import org.modelmap.sample.model.SampleModels;
+import static org.modelmap.core.dsl.DSL.matchAll;
+import static org.modelmap.sample.field.SampleFieldIdInfo.accountCountry;
+import static org.modelmap.sample.field.SampleFieldIdInfo.accountLanguage;
+import static org.modelmap.sample.field.SampleFieldIdInfo.accountPhoneNumber;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.modelmap.core.dsl.DSL;
+import org.modelmap.core.dsl.lang.StepCondition;
+import org.modelmap.core.dsl.lang.ValidationRule;
+import org.modelmap.sample.model.*;
 import org.modelmap.sample.validation.Registry;
 import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
 import org.openjdk.jmh.logic.BlackHole;
 
 public class BenchmarkRule {
 
+    private static final ValidationRule ACCOUNT_VALID_COUNTRY_20 = DSL.when(matchAll(conditions(20))).validate();
+    private static final ValidationRule ACCOUNT_VALID_COUNTRY_40 = DSL.when(matchAll(conditions(40))).validate();
+    private static final ValidationRule ACCOUNT_VALID_COUNTRY_60 = DSL.when(matchAll(conditions(60))).validate();
+    private static final ValidationRule ACCOUNT_VALID_COUNTRY_80 = DSL.when(matchAll(conditions(80))).validate();
+    private static final ValidationRule ACCOUNT_VALID_COUNTRY_100 = DSL.when(matchAll(conditions(100))).validate();
+
+    @GenerateMicroBenchmark
     public void valid_email(BlackHole blackhole) {
         boolean valid = Registry.ACCOUNT_VALID_EMAIL.executeOn(SampleModels.wrapper()).isValid();
         if (blackhole != null) {
@@ -14,6 +32,7 @@ public class BenchmarkRule {
         }
     }
 
+    @GenerateMicroBenchmark
     public void valid_country(BlackHole blackhole) {
         boolean valid = Registry.ACCOUNT_VALID_COUNTRY.executeOn(SampleModels.wrapper()).isValid();
         if (blackhole != null) {
@@ -21,44 +40,48 @@ public class BenchmarkRule {
         }
     }
 
-    @GenerateMicroBenchmark
     public void valid_country_20(BlackHole blackhole) {
-        boolean valid = Registry.ACCOUNT_VALID_COUNTRY_20.executeOn(SampleModels.wrapper()).isValid();
+        boolean valid = ACCOUNT_VALID_COUNTRY_20.executeOn(SampleModels.wrapper()).isValid();
         if (blackhole != null) {
             blackhole.consume(valid);
         }
     }
 
-    @GenerateMicroBenchmark
     public void valid_country_40(BlackHole blackhole) {
-        boolean valid = Registry.ACCOUNT_VALID_COUNTRY_40.executeOn(SampleModels.wrapper()).isValid();
+        boolean valid = ACCOUNT_VALID_COUNTRY_40.executeOn(SampleModels.wrapper()).isValid();
         if (blackhole != null) {
             blackhole.consume(valid);
         }
     }
 
-    @GenerateMicroBenchmark
     public void valid_country_60(BlackHole blackhole) {
-        boolean valid = Registry.ACCOUNT_VALID_COUNTRY_60.executeOn(SampleModels.wrapper()).isValid();
+        boolean valid = ACCOUNT_VALID_COUNTRY_60.executeOn(SampleModels.wrapper()).isValid();
         if (blackhole != null) {
             blackhole.consume(valid);
         }
     }
 
-    @GenerateMicroBenchmark
     public void valid_country_80(BlackHole blackhole) {
-        boolean valid = Registry.ACCOUNT_VALID_COUNTRY_80.executeOn(SampleModels.wrapper()).isValid();
+        boolean valid = ACCOUNT_VALID_COUNTRY_80.executeOn(SampleModels.wrapper()).isValid();
         if (blackhole != null) {
             blackhole.consume(valid);
         }
     }
 
-    @GenerateMicroBenchmark
     public void valid_country_100(BlackHole blackhole) {
-        boolean valid = Registry.ACCOUNT_VALID_COUNTRY_100.executeOn(SampleModels.wrapper()).isValid();
+        boolean valid = ACCOUNT_VALID_COUNTRY_100.executeOn(SampleModels.wrapper()).isValid();
         if (blackhole != null) {
             blackhole.consume(valid);
         }
+    }
+
+    private static StepCondition[] conditions(int count) {
+        return IntStream.range(0, count)
+                        .mapToObj(operand -> accountCountry().eq(Country.FR)
+                                        .and(accountLanguage().eq(Language.FR))
+                                        .and(accountPhoneNumber().startsWith("+33")))
+                        .collect(Collectors.toList())
+                        .toArray(new StepCondition[] {});
     }
 
 }
--- sample/validation/src/main/java/org/modelmap/sample/validation/Registry.java
+++ sample/validation/src/main/java/org/modelmap/sample/validation/Registry.java
@@ -1,5 +1,6 @@
 package org.modelmap.sample.validation;
 
+import static org.modelmap.core.dsl.DSL.matchAll;
 import static org.modelmap.core.dsl.DSL.matchAny;
 import static org.modelmap.sample.field.SampleFieldIdInfo.accountCountry;
 import static org.modelmap.sample.field.SampleFieldIdInfo.accountEmail;
@@ -22,25 +23,20 @@ public enum Registry implements RuleRegistry {
     public static final ValidationRule ACCOUNT_VALID_EMAIL;
     public static final ValidationRule ACCOUNT_VALID_EMAIL_SIZE;
     public static final ValidationRule ACCOUNT_VALID_COUNTRY;
-    public static final ValidationRule ACCOUNT_VALID_COUNTRY_20;
-    public static final ValidationRule ACCOUNT_VALID_COUNTRY_40;
-    public static final ValidationRule ACCOUNT_VALID_COUNTRY_60;
-    public static final ValidationRule ACCOUNT_VALID_COUNTRY_80;
-    public static final ValidationRule ACCOUNT_VALID_COUNTRY_100;
+
 
     static {
         ACCOUNT_VALID_EMAIL = DSL.when(accountEmail().matches("\\w+[@]\\w+\\.com")
                         .or(accountEmail().matches("\\w+[@]\\w+\\.fr")))
                         .validate()
                         .withMessage("email finishes with .com or .fr")
-                        .registerOn(ACCOUNT, id);
+                        .registerOn(ACCOUNT);
 
         ACCOUNT_VALID_EMAIL_SIZE = DSL.when(accountEmail().length().between(5, 200))
                         .validate()
                         .registerOn(ACCOUNT);
 
-        DSL.when(matchAny(
-                        accountCountry().eq(Country.FR)
+        ACCOUNT_VALID_COUNTRY = DSL.when(matchAny(accountCountry().eq(Country.FR)
                                         .and(accountLanguage().eq(Language.FR))
                                         .and(accountPhoneNumber().startsWith("+33")),
                         accountCountry().eq(Country.UK)
@@ -48,6 +44,8 @@ public enum Registry implements RuleRegistry {
                                         .and(accountPhoneNumber().startsWith("+45"))))
                         .validate()
                         .registerOn(ACCOUNT);
+
+
     }
 
     private List<ValidationRule> rules = new ArrayList<>();
@@ -60,15 +58,6 @@ public enum Registry implements RuleRegistry {
     @Override
     public Stream<ValidationRule> stream() {
         return rules.stream();
-    }
-
-    private static StepCondition[] conditions(int count) {
-        return IntStream.range(0, count)
-                        .mapToObj(operand -> accountCountry().eq(Country.UK)
-                                        .and(accountLanguage().eq(Language.EN))
-                                        .and(accountPhoneNumber().startsWith("+45")))
-                        .collect(Collectors.toList())
-                        .toArray(new StepCondition[] {});
     }
 
 }
