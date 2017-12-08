96d78e44abb5751ff48ab690b4c038b99bef8e43
Alexandre DuBreuil <adu@lesfurets.com>
2017-12-08 11:40:39 +0100

    Refactor rules to static field pattern

--- sample/validation/src/main/java/io/doov/sample/validation/Rules.java
+++ sample/validation/src/main/java/io/doov/sample/validation/Rules.java
@@ -17,7 +17,6 @@ package io.doov.sample.validation;
 
 import static io.doov.core.dsl.DOOV.count;
 import static io.doov.core.dsl.DOOV.matchAll;
-import static io.doov.core.dsl.time.LocalDateSuppliers.firstDayOfThisYear;
 import static io.doov.core.dsl.time.LocalDateSuppliers.today;
 import static io.doov.core.dsl.time.TemporalAdjuster.firstDayOfYear;
 import static io.doov.sample.field.SampleFieldIdInfo.*;
@@ -25,6 +24,7 @@ import static java.time.temporal.ChronoUnit.DAYS;
 
 import io.doov.core.dsl.DOOV;
 import io.doov.core.dsl.impl.DefaultRuleRegistry;
+import io.doov.core.dsl.lang.ValidationRule;
 import io.doov.sample.model.Country;
 
 public class Rules extends DefaultRuleRegistry {
@@ -32,64 +32,73 @@ public class Rules extends DefaultRuleRegistry {
     public static void init() {
     }
 
-    static {
-        DOOV.when(accountEmail().matches("\\w+[@]\\w+\\.com")
-                .or(accountEmail().matches("\\w+[@]\\w+\\.fr")))
-                .validate()
-                .withMessage("email finishes with .com or .fr")
-                .registerOn(REGISTRY_DEFAULT);
+    private static ValidationRule RULE_EMAIL = DOOV
+            .when(accountEmail().matches("\\w+[@]\\w+\\.com")
+                    .or(accountEmail().matches("\\w+[@]\\w+\\.fr")))
+            .validate()
+            .withMessage("email finishes with .com or .fr")
+            .registerOn(REGISTRY_DEFAULT);
 
-        DOOV.when(matchAll(userBirthdate().ageAt(today()).greaterOrEquals(18),
-                accountEmail().length().lesserOrEquals(configurationMaxEmailSize()),
-                accountCountry().eq(Country.FR).and(accountPhoneNumber().startsWith("+33"))))
-                .validate()
-                .registerOn(REGISTRY_DEFAULT);
+    private static final ValidationRule RULE_ACCOUNT = DOOV
+            .when(matchAll(
+                    userBirthdate().ageAt(today()).greaterOrEquals(18),
+                    accountEmail().length().lesserOrEquals(configurationMaxEmailSize()),
+                    accountCountry().eq(Country.FR).and(accountPhoneNumber().startsWith("+33"))))
+            .validate()
+            .registerOn(REGISTRY_DEFAULT);
 
-        DOOV.when(userBirthdate().ageAt(today()).greaterOrEquals(18)
-                .and(accountEmail().length().lesserOrEquals(configurationMaxEmailSize()))
-                .and(accountCountry().eq(Country.FR))
-                .and(accountPhoneNumber().startsWith("+33")))
-                .validate()
-                .registerOn(REGISTRY_DEFAULT);
+    private static final ValidationRule RULE_ACCOUNT_2 = DOOV
+            .when(userBirthdate().ageAt(today()).greaterOrEquals(18)
+                    .and(accountEmail().length().lesserOrEquals(configurationMaxEmailSize()))
+                    .and(accountCountry().eq(Country.FR))
+                    .and(accountPhoneNumber().startsWith("+33")))
+            .validate()
+            .registerOn(REGISTRY_DEFAULT);
 
-        DOOV.when(count(userFirstName().isNotNull(),
-                userLastName().isNotNull().and(userLastName().matches("[A-Z]+")))
-                .greaterOrEquals(0))
-                .validate()
-                .withShortCircuit(false)
-                .registerOn(REGISTRY_DEFAULT);
+    private static final ValidationRule RULE_USER = DOOV
+            .when(count(
+                    userFirstName().isNotNull(),
+                    userLastName().isNotNull().and(userLastName().matches("[A-Z]+")))
+                    .greaterOrEquals(0))
+            .validate()
+            .withShortCircuit(false)
+            .registerOn(REGISTRY_DEFAULT);
 
-        DOOV.when(userLastName().isNotNull().and(userLastName().matches("[A-Z]+")
-                .and(count(accountPhoneNumber().isNotNull(), accountEmail().isNotNull())
-                        .greaterThan(0))))
-                .validate()
-                .registerOn(REGISTRY_DEFAULT);
+    private static final ValidationRule RULE_USER_2 = DOOV
+            .when(userLastName().isNotNull().and(userLastName().matches("[A-Z]+")
+                    .and(count(
+                            accountPhoneNumber().isNotNull(),
+                            accountEmail().isNotNull())
+                            .greaterThan(0))))
+            .validate()
+            .registerOn(REGISTRY_DEFAULT);
 
-        DOOV.when(userBirthdate().ageAt(accountCreationDate()).greaterOrEquals(18))
-                .validate()
-                .registerOn(REGISTRY_DEFAULT);
+    private static final ValidationRule RULE_USER_ADULT = DOOV
+            .when(userBirthdate().ageAt(accountCreationDate()).greaterOrEquals(18))
+            .validate()
+            .registerOn(REGISTRY_DEFAULT);
 
-        DOOV.when(userBirthdate().ageAt(accountCreationDate().with(firstDayOfYear())).greaterOrEquals(18))
-                .validate()
-                .registerOn(REGISTRY_DEFAULT);
+    private static final ValidationRule RULE_USER_ADULT_FIRSTDAY = DOOV
+            .when(userBirthdate().ageAt(accountCreationDate().with(firstDayOfYear())).greaterOrEquals(18))
+            .validate()
+            .registerOn(REGISTRY_DEFAULT);
 
-        DOOV.when(userBirthdate().ageAt(firstDayOfThisYear()).greaterOrEquals(18))
-                .validate()
-                .registerOn(REGISTRY_DEFAULT);
+    private static final ValidationRule RULE_FIRST_NAME = DOOV
+            .when(matchAll(userFirstName().mapToInt(name -> 1).eq(1)))
+            .validate()
+            .registerOn(REGISTRY_DEFAULT);
 
-        DOOV.when(matchAll(userFirstName().mapToInt(name -> 1).eq(1)))
-                .validate()
-                .registerOn(REGISTRY_DEFAULT);
+    private static final ValidationRule RULE_ID = DOOV
+            .when(userId().isNotNull())
+            .validate()
+            .registerOn(REGISTRY_DEFAULT);
 
-        DOOV.when(userId().isNotNull())
-                .validate()
-                .registerOn(REGISTRY_DEFAULT);
+    private static final ValidationRule RULE_AGE = DOOV
+            .when(userBirthdate().ageAt(today()).greaterOrEquals(18)).validate()
+            .registerOn(REGISTRY_DEFAULT);
 
-        DOOV.when(userBirthdate().ageAt(today()).greaterOrEquals(18)).validate()
-                .registerOn(REGISTRY_DEFAULT);
-
-        DOOV.when(userBirthdate().after(userBirthdate().minus(1, DAYS))).validate()
-                .registerOn(REGISTRY_DEFAULT);
-    }
+    private static final ValidationRule RULE_AGE_2 = DOOV
+            .when(userBirthdate().after(userBirthdate().minus(1, DAYS))).validate()
+            .registerOn(REGISTRY_DEFAULT);
 
 }
