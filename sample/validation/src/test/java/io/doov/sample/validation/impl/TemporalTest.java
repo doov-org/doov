f07f3c9733d712437df8d66d65ddf23fe579f010
Alexandre DuBreuil <adu@lesfurets.com>
2017-12-11 11:38:05 +0100

    Update temporal tests

--- sample/validation/src/test/java/io/doov/sample/validation/impl/TemporalTest.java
+++ sample/validation/src/test/java/io/doov/sample/validation/impl/TemporalTest.java
@@ -23,7 +23,6 @@ import static io.doov.core.dsl.time.TemporalAdjuster.ofDateAdjuster;
 import static io.doov.sample.field.SampleFieldIdInfo.accountCreationDate;
 import static io.doov.sample.field.SampleFieldIdInfo.configurationMinAge;
 import static io.doov.sample.field.SampleFieldIdInfo.userBirthdate;
-import static java.time.LocalDate.now;
 import static java.time.temporal.ChronoUnit.DAYS;
 import static java.time.temporal.ChronoUnit.YEARS;
 
@@ -33,91 +32,98 @@ import java.time.temporal.TemporalAdjusters;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import io.doov.core.FieldModel;
+import io.doov.core.dsl.DslModel;
 import io.doov.sample.model.*;
 
 public class TemporalTest {
 
-    private FieldModel wrapper;
+    private DslModel model;
+    private Configuration configuration;
     private Account account;
     private User user;
 
     @BeforeEach
     public void before() {
-        SampleModel sample = SampleModels.sample();
-        wrapper = new SampleModelWrapper(sample);
-        account = sample.getAccount();
-        user = sample.getUser();
-
-        user.setBirthDate(now());
-        account.setCreationDate(now());
+        SampleModel sampleModel = new SampleModel();
+        sampleModel.setUser(user = new User());
+        sampleModel.setAccount(account = new Account());
+        sampleModel.setConfiguration(configuration = new Configuration());
+        model = new SampleModelWrapper(sampleModel);
     }
 
     @Test
     public void should_temporal_same_as_local_date_operation_eq() {
-        assertThat(userBirthdate().eq(now())).validates(wrapper);
-        assertThat(userBirthdate().notEq(now())).doesNotValidate(wrapper);
+        user.setBirthDate(LocalDate.now());
+
+        assertThat(userBirthdate().eq(LocalDate.now())).validates(model);
+        assertThat(userBirthdate().notEq(LocalDate.now())).doesNotValidate(model);
     }
 
     @Test
     public void should_temporal_same_as_local_date_operation_with() {
-        assertThat(userBirthdate().with(firstDayOfMonth()).eq(now().with(TemporalAdjusters.firstDayOfMonth())))
-                .validates(wrapper);
-        assertThat(userBirthdate().with(firstDayOfMonth()).notEq(now().with(TemporalAdjusters.firstDayOfMonth())))
-                .doesNotValidate(wrapper);
+        user.setBirthDate(LocalDate.now());
+
+        assertThat(userBirthdate().with(firstDayOfMonth())
+                .eq(LocalDate.now().with(TemporalAdjusters.firstDayOfMonth())))
+                .validates(model);
+        assertThat(userBirthdate().with(firstDayOfMonth())
+                .notEq(LocalDate.now().with(TemporalAdjusters.firstDayOfMonth())))
+                .doesNotValidate(model);
     }
 
     @Test
     public void should_temporal_same_as_local_date_operation_plus_and_minus() {
-        LocalDate birthDate = now().minus(18, YEARS);
+        LocalDate birthDate = LocalDate.now().minus(18, YEARS);
         user.setBirthDate(birthDate);
+        configuration.setMinAge(18);
 
-        assertThat(userBirthdate().eq(birthDate)).validates(wrapper);
-        assertThat(userBirthdate().minus(1, DAYS).eq(birthDate.minus(1, DAYS))).validates(wrapper);
-        assertThat(userBirthdate().plus(1, DAYS).eq(birthDate.plus(1, DAYS))).validates(wrapper);
+        assertThat(userBirthdate().eq(birthDate)).validates(model);
+        assertThat(userBirthdate().minus(1, DAYS).eq(birthDate.minus(1, DAYS))).validates(model);
+        assertThat(userBirthdate().plus(1, DAYS).eq(birthDate.plus(1, DAYS))).validates(model);
         assertThat(userBirthdate().minus(configurationMinAge(), YEARS).eq(birthDate.minus(18, YEARS)))
-                .validates(wrapper);
+                .validates(model);
         assertThat(userBirthdate().plus(configurationMinAge(), YEARS).eq(birthDate.plus(18, YEARS)))
-                .validates(wrapper);
+                .validates(model);
     }
 
     @Test
     public void should_temporal_same_as_local_date_operation_years_between() {
-        LocalDate birthDate = now().minus(18, YEARS);
+        LocalDate birthDate = LocalDate.now().minus(18, YEARS);
         user.setBirthDate(birthDate);
+        account.setCreationDate(LocalDate.now());
 
-        assertThat(userBirthdate().yearsBetween(now()).eq(18)).validates(wrapper);
-        assertThat(userBirthdate().plus(1, DAYS).yearsBetween(now()).eq(18)).doesNotValidate(wrapper);
-        assertThat(userBirthdate().plus(1, DAYS).minus(1, DAYS).yearsBetween(now()).eq(18)).validates(wrapper);
-        assertThat(userBirthdate().yearsBetween(accountCreationDate()).eq(18)).validates(wrapper);
+        assertThat(userBirthdate().yearsBetween(LocalDate.now()).eq(18)).validates(model);
+        assertThat(userBirthdate().plus(1, DAYS).yearsBetween(LocalDate.now()).eq(18)).doesNotValidate(model);
+        assertThat(userBirthdate().plus(1, DAYS).minus(1, DAYS).yearsBetween(LocalDate.now()).eq(18)).validates(model);
+        assertThat(userBirthdate().yearsBetween(accountCreationDate()).eq(18)).validates(model);
 
         user.setBirthDate(birthDate.plus(1, DAYS));
-        assertThat(userBirthdate().yearsBetween(now()).eq(17)).validates(wrapper);
-        assertThat(userBirthdate().minus(1, DAYS).yearsBetween(now()).eq(18)).validates(wrapper);
-        assertThat(userBirthdate().yearsBetween(now().plus(1, DAYS)).eq(18)).validates(wrapper);
+        assertThat(userBirthdate().yearsBetween(LocalDate.now()).eq(17)).validates(model);
+        assertThat(userBirthdate().minus(1, DAYS).yearsBetween(LocalDate.now()).eq(18)).validates(model);
+        assertThat(userBirthdate().yearsBetween(LocalDate.now().plus(1, DAYS)).eq(18)).validates(model);
 
         user.setBirthDate(LocalDate.of(2000, 1, 1));
-        assertThat(userBirthdate().yearsBetween(LocalDate.of(2018, 1, 1)).eq(18)).validates(wrapper);
-        assertThat(userBirthdate().yearsBetween(LocalDate.of(2018, 1, 2)).eq(18)).validates(wrapper);
-        assertThat(userBirthdate().yearsBetween(LocalDate.of(2017, 12, 31)).eq(17)).validates(wrapper);
+        assertThat(userBirthdate().yearsBetween(LocalDate.of(2018, 1, 1)).eq(18)).validates(model);
+        assertThat(userBirthdate().yearsBetween(LocalDate.of(2018, 1, 2)).eq(18)).validates(model);
+        assertThat(userBirthdate().yearsBetween(LocalDate.of(2017, 12, 31)).eq(17)).validates(model);
 
         account.setCreationDate(LocalDate.of(2018, 1, 1));
-        assertThat(userBirthdate().yearsBetween(accountCreationDate()).eq(18)).validates(wrapper);
+        assertThat(userBirthdate().yearsBetween(accountCreationDate()).eq(18)).validates(model);
     }
 
     @Test
     public void should_temporal_same_as_local_date_operation_with_chained() {
-        LocalDate birthDate = now().minus(18, YEARS);
+        LocalDate birthDate = LocalDate.now().minus(18, YEARS);
         user.setBirthDate(birthDate);
 
         assertThat(userBirthdate().with(firstDayOfYear())
-                .yearsBetween(now().with(TemporalAdjusters.firstDayOfYear()))
+                .yearsBetween(LocalDate.now().with(TemporalAdjusters.firstDayOfYear()))
                 .eq(18))
-                .validates(wrapper);
+                .validates(model);
         assertThat(userBirthdate().with(firstDayOfNextYear()).with(ofDateAdjuster(d -> d.withDayOfMonth(15)))
-                .yearsBetween(now().with(TemporalAdjusters.firstDayOfNextYear())
+                .yearsBetween(LocalDate.now().with(TemporalAdjusters.firstDayOfNextYear())
                         .with(TemporalAdjusters.ofDateAdjuster(d -> d.withDayOfMonth(15))))
-                .eq(18)).validates(wrapper);
+                .eq(18)).validates(model);
     }
 
 }
