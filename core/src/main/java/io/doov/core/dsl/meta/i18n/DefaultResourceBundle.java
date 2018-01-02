e17f90b53bdbab076f66dfeaba9146b691bfb386
gdu <gdigugli@gmail.com>
2018-01-02 12:20:01 +0100

    prevent resource bundle MissingResourceException

--- core/src/main/java/io/doov/core/dsl/meta/i18n/DefaultResourceBundle.java
+++ core/src/main/java/io/doov/core/dsl/meta/i18n/DefaultResourceBundle.java
@@ -5,22 +5,26 @@ package io.doov.core.dsl.meta.i18n;
 
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
 
 import io.doov.core.dsl.meta.Operator;
 
 public enum DefaultResourceBundle {
     BUNDLE;
-    
+
     private static final String baseName = "io.doov.core.dsl.meta.i18n.OperatorResourceBundle";
-    
+
     public String get(Operator operator) {
         return get(operator, Locale.getDefault());
     }
-    
+
     public String get(Operator operator, Locale locale) {
-        final String value =  ResourceBundle.getBundle(baseName, locale).getString(operator.name());
+        final String value;
+        try {
+            value = ResourceBundle.getBundle(baseName, locale).getString(operator.name());
+        } catch (MissingResourceException e) {
+            return operator.readable();
+        }
         if (isEmpty(value))
             return operator.readable();
         return value;
