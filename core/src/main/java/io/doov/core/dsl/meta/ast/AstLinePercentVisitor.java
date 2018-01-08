a76c0cafc2f84829711501f0e5b2dab80e73c293
YFouquer <yohann.fouquer@lesfurets.com>
2018-01-08 12:44:21 +0100

    :sparkles: LinePercent visitor

---
+++ core/src/main/java/io/doov/core/dsl/meta/ast/AstLinePercentVisitor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) by Courtanet, All Rights Reserved.
+ */
+package io.doov.core.dsl.meta.ast;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import io.doov.core.dsl.meta.*;
+
+public class AstLinePercentVisitor extends AstLineVisitor {
+    private static final NumberFormat formatter = new DecimalFormat("###.#");
+
+    public AstLinePercentVisitor(StringBuilder stringBuilder, ResourceProvider bundle, Locale locale) {
+        super(stringBuilder, bundle, locale);
+    }
+
+    @Override
+    public void startMetadata(NaryMetadata metadata, int depth) {
+        sb.append(formatCurrentIndent());
+        sb.append(bundle.get(metadata.getOperator(), locale));
+        sb.append(formatNewLine());
+        sb.append(percentage(metadata));
+        sb.append("[");
+    }
+
+    @Override
+    protected String formatLeafMetadata(LeafMetadata metadata) {
+        return super.formatLeafMetadata(metadata) + percentage(metadata);
+    }
+
+    private String percentage(PredicateMetadata metadata) {
+        int t = metadata.trueEvalCount();
+        int f = metadata.falseEvalCount();
+
+        if (f == 0 && t == 0) {
+            return " [n/a]";
+        }
+        else{
+            return " [" + formatter.format((t / ((double) t + f))*100) + "]";
+        }
+    }
+}
