032c6af81279200109af85532581bc9b3cde538e
jrudat <justin.rudat@lesfurets.com>
2018-09-13 11:54:57 +0200

    adding a mannageOpe method and modifying LeafMD visit method
    managing the match all/any/none writing
    
    managing the count method

--- core/src/main/java/io/doov/core/dsl/meta/ast/AstProceduralJSVisitor.java
+++ core/src/main/java/io/doov/core/dsl/meta/ast/AstProceduralJSVisitor.java
@@ -7,8 +7,7 @@ import static io.doov.core.dsl.meta.DefaultOperator.*;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 import io.doov.core.dsl.lang.*;
 import io.doov.core.dsl.meta.*;
@@ -19,6 +18,9 @@ public class AstProceduralJSVisitor extends AbstractAstVisitor {
     protected final OutputStream ops;
     protected final ResourceProvider bundle;
     protected final Locale locale;
+    private int parenthese_depth = 0;
+    private int start_with_count = 0;
+    private int nary_args_count = 0 ;
 
     public AstProceduralJSVisitor(OutputStream ops, ResourceProvider bundle, Locale locale) {
         this.ops = ops;
@@ -29,334 +31,268 @@ public class AstProceduralJSVisitor extends AbstractAstVisitor {
     }
 
     @Override
-    public void visitMetadata(Metadata metadata, int depth) {
-        write(metadata.readable());
+    public void startMetadata(BinaryMetadata metadata,int depth){
+        write("(");
     }
 
     @Override
-    public void visitMetadata(UnaryMetadata metadata, int depth) {
-        switch (metadata.getOperator().readable()) {
-            case "not":
-                write("!(" + metadata.readable() + ")");
+    public void visitMetadata(BinaryMetadata metadata,int depth){
+        switch((DefaultOperator)metadata.getOperator()){
+            case and:
+                write(" && ");
+                break;
+            case or:
+                write(" || ");
+                break;
+            case greater_than:
+                write(" > ");
+                break;
+            case greater_or_equals:
+                write(" >= ");
+                break;
+            case lesser_than:
+                write(" < ");
+                break;
+            case lesser_or_equals:
+                write(" <= ");
+            case equals:
+                write(" == ");
+                break;
+            case not_equals:
+                write(" != ");
                 break;
 
-            case "as a number":
-                write("parseFloat(" + metadata.readable() + ")");
-                break;
-
-            case "as a string":
-                write("String(" + metadata.readable() + ")");
-                break;
-
-            case "is null":
-                write("( null == " + metadata.readable() + ")");
-                break;
-
-            case "is not null":
-                write("(null != " + metadata.readable() + ")");
-                break;
-
-            case "when":
-                write("if(" + metadata.readable() + "){ return true; }");
-                break;
-
-            case "today":
-                write("moment()");
-                break;
-
-            case "today minus":
-                write("moment().subtract(" + metadata.readable() + ",'days')");
-                break;
-
-            case "today plus":
-                write("moment().add(" + metadata.readable() + ",'days')");
-                break;
-
-            case "is empty":
-                write("(" + metadata.readable() + ".length == 0 )");
-                break;
-
-            case "is not empty":
-                write("(" + metadata.readable() + ".length != 0 )");
-                break;
-
-            default:
-                write("Un-Error"); // will create a syntax error in js
-                // if unarymetadata operator unrecognized
-                break;
-        }
-
-    }
-
-    @Override
-    public void endMetadata(UnaryMetadata metadata, int depth) {
-        try {
-            ops.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
     @Override
-    public void visitMetadata(BinaryMetadata metadata, int depth) {
-        switch (metadata.getOperator().readable()) {
-            case "and":
-                write("(" + metadata.getLeft().readable() + " && " + metadata.getRight() + ")");
-                break;
+    public void endMetadata(BinaryMetadata metadata,int depth){
+        write(")");
+    }
 
-            case "or":
-                write("(" + metadata.getLeft().readable() + " || " + metadata.getRight() + ")");
-                break;
+    @Override
+    public void startMetadata(NaryMetadata metadata,int depth){
+        if(metadata.getOperator()==match_none) {
+            write("!");
+        }
+        if(metadata.getOperator()==count){
+            write("function(){ var count=0; var array-test = [");
+        }
+    }
 
-            case "=":
-                write("(" + metadata.getLeft().readable() + " == " + metadata.getRight().readable() + ")");
-                break;
 
-            case "!=":
-                write("(" + metadata.getLeft().readable() + " != " + metadata.getRight().readable() + ")");
-                break;
-
-            case "xor":
-                write("((!" + metadata.getLeft() + " && " + metadata.getRight()
-                        + ") || (" + metadata.getLeft() + " && !" + metadata.getRight() + "))");
-                break;
-
-            case "<":
-                write("(" + metadata.getLeft() + " < " + metadata.getRight() + ")");
-                break;
-
-            case "<=":
-                write("(" + metadata.getLeft() + " <= " + metadata.getRight() + ")");
-                break;
-
-            case ">":
-                write("(" + metadata.getLeft() + " > " + metadata.getRight() + ")");
-                break;
-
-            case ">=":
-                write("(" + metadata.getLeft() + " >= " + metadata.getRight() + ")");
-                break;
-
-            case "minus":
-                write(metadata.getLeft() + ".subtract(" + metadata.getRight() + ",'days')");
-                break;
-
-            case "plus":
-                write(metadata.getLeft() + ".add(" + metadata.getRight() + ",'days')");
-                break;
-
-            case "after":
-                write("moment(" + metadata.getLeft() + ").isAfter(" + metadata.getRight() + ")");
-                break;
-
-            case "after or equals":
-                write("moment(" + metadata.getLeft() + ").isSameOrAfter(" + metadata.getRight() + ")");
-                break;
-
-            case "before":
-                write("moment(" + metadata.getLeft() + ").isBefore(" + metadata.getRight() + ")");
-                break;
-
-            case "before or equals":
-                write("moment(" + metadata.getLeft() + ").isSameOrBefore(" + metadata.getRight() + ")");
-                break;
-
-            case "has size":
-                write("(" + metadata.getLeft() + ".length == " + metadata.getRight() + ")");
-                break;
-
-            case "has not size":
-                write("(" + metadata.getLeft() + ".length != " + metadata.getRight() + ")");
-                break;
-
-            case "age at":
-                write("moment.duration(" + metadata.getLeft() + ".diff(" + metadata.getRight() + ")).asYears()");
-                break;
-
-            case "starts with":
-                write("String(" + metadata.getLeft() + ").match(\"*\"+" + metadata.getRight() + ")");
-                break;
-
-            case "ends with":
-                write("String(" + metadata.getLeft() + ").match(" + metadata.getRight() + "+\"*\")");
-                break;
-
-            case "is":
-                write("(" + metadata.getLeft() + " === " + metadata.getRight() + ")");
-                break;
-
-            default:
-                write("Bin-Error");// will create a syntax error in js
-                // if binarymetadata operator unrecognized
-                break;
+    @Override
+    public void visitMetadata(NaryMetadata metadata,int depth){
+        nary_args_count++;
+        if(nary_args_count!=metadata.children().size()) {
+            switch((DefaultOperator)metadata.getOperator()){
+                case match_any:
+                    write(" || ");
+                    break;
+                case match_all:
+                    write(" && ");
+                    break;
+                case match_none:
+                    write(" && !");
+                    break;
+                case count:
+                    write(", ");
+                    break;
+            }
         }
     }
 
     @Override
-    public void endMetadata(BinaryMetadata metadata, int depth) {
-        try {
-            ops.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public void endMetadata(NaryMetadata metadata,int depth){
+        nary_args_count = 0 ;
+        if(metadata.getOperator()==count){
+            write("]; array-test.forEach(function(item){" +
+                    "if(item){count++;}} return count;}");
         }
+
     }
 
     @Override
     public void visitMetadata(LeafMetadata metadata, int depth) {
-
-
-        System.out.println("----");
-        metadata.stream().peek(System.out::print).forEach(element -> {
+        ArrayList<Element> date_field = new ArrayList<>();
+        final int[] is_age_at = { 0 };
+        metadata.stream().forEach(element -> {
             switch (element.getType()) {
-
                 case FIELD:
-                    System.out.println("         FIELD");
+                    write(element.toString());
                     break;
                 case OPERATOR:
-                    System.out.println("         OPERATOR");
+                    manageOperator((DefaultOperator)element.getReadable());
                     break;
                 case VALUE:
-                    System.out.println("         VALUE");
+                    write(element.toString());
                     break;
                 case STRING_VALUE:
-                    System.out.println("         STRING_VALUE");
+                    write("\""+element+"\"");
                     break;
                 case PARENTHESIS_LEFT:
-                    System.out.println("         PARENTHESIS_LEFT");
+                    write(element.toString());
                     break;
                 case PARENTHESIS_RIGHT:
-                    System.out.println("         PARENTHESIS_RIGHT");
+                    write(element.toString());
                     break;
                 case TEMPORAL_UNIT:
-                    System.out.println("         TEMPORAL_UNIT");
+                    write(element.toString());
                     break;
                 case UNKNOWN:
-                    System.out.println("         UNKNOWN");
+                    write("Unknown "+element.toString());
                     break;
             }
         });
-        System.out.println("----");
-
-
-        //        switch (metadata.type()){
-        //            case FIELD_PREDICATE_MATCH_ANY:
-        //                List<Metadata> children = metadata.children();
-        //                write("match("+", "+")");
-        //                break;
-        //            default:
-        //                write("Leaf-Error");// will create a syntax error in js
-        //                 if leafmetadata operator unrecognized
-        //                break;
-        //        }
-    }
-
-    @Override
-    public void endMetadata(LeafMetadata metadata, int depth) {
-        try {
-            ops.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        if(start_with_count>0){
+            write("+\"*\"");
+            start_with_count--;
+        }
+        if(parenthese_depth>0) {
+            write(")");
+            parenthese_depth--;
         }
     }
 
-    @Override
-    public void visitMetadata(NaryMetadata metadata, int depth) {
-        List<Metadata> children = metadata.children();
-        write("function(){");
-        write("var array-test = [");
-        for (Metadata mtd : children) {
-            write(mtd.readable());
-            if (children.indexOf(mtd) != children.size() - 1) {
-                write(", ");
-            }
+    public void manageOperator(DefaultOperator element){
+
+        switch(element){
+            case rule:
+                break;
+            case validate:
+                break;
+            case empty:
+                break;
+            case sum:
+                break;
+            case min:
+                break;
+            case not:
+                write("!");
+                break;
+            case always_true:
+                break;
+            case always_false:
+                break;
+            case times:
+                break;
+            case equals:
+                write(" == ");
+                break;
+            case not_equals:
+                write(" != ");
+                break;
+            case is_null:
+                write(" == null ");
+                break;
+            case is_not_null:
+                write(" != null ");
+                break;
+            case as_a_number:
+                break;
+            case as_string:
+                break;
+            case as:
+                break;
+            case with:
+                break;
+            case minus:
+                break;
+            case plus:
+                break;
+            case after:
+                break;
+            case after_or_equals:
+                break;
+            case age_at:
+
+                break;
+            case before:
+                break;
+            case before_or_equals:
+                break;
+            case matches:
+                write(".match(");
+                parenthese_depth++;
+                break;
+            case contains:
+                break;
+            case starts_with:
+                write(".match(\"^\"+");
+                start_with_count++;
+                parenthese_depth++;
+                break;
+            case ends_with:
+                write(".match(\"*\"+");
+                parenthese_depth++;
+                break;
+            case greater_than:
+                write(" > ");
+                break;
+            case greater_or_equals:
+                write(" >= ");
+                break;
+            case xor:
+                break;
+            case is:
+                write(" === ");
+                break;
+            case lesser_than:
+                write(" < ");
+                break;
+            case lesser_or_equals:
+                write(" <= ");
+                break;
+            case has_not_size:
+                break;
+            case has_size:
+                break;
+            case is_empty:
+                break;
+            case is_not_empty:
+                break;
+            case length_is:
+                break;
+            case today:
+                write("moment()");
+                break;
+            case today_plus:
+                break;
+            case today_minus:
+                break;
+            case first_day_of_this_month:
+                break;
+            case first_day_of_this_year:
+                break;
+            case last_day_of_this_month:
+                break;
+            case last_day_of_this_year:
+                break;
+            case first_day_of_month:
+                break;
+            case first_day_of_next_month:
+                break;
+            case first_day_of_year:
+                break;
+            case first_day_of_next_year:
+                break;
+            case last_day_of_month:
+                break;
+            case last_day_of_year:
+                break;
+
         }
-        write("];\n");
-        switch (metadata.getOperator().readable()) {
-            case "count":
-                write("var nb-true = 0;" +
-                        "array-test.forEach(function(item){" +
-                        "if(item){" +
-                        "nb-true++;" +
-                        "}" +
-                        "});" +
-                        "return nb-true;");
-                break;
-            case "match all":
-                write("var test = 0 ;");
-                write("array-test.forEach(function(item){" +
-                        "if(!item){" +
-                        "test = 1;}" +
-                        "});");
-                write("if(test != 0 ){" +
-                        "return false;}" +
-                        "return true;");
-                break;
-            case "match none":
-                write("var test = 0 ;");
-                write("array-test.forEach(function(item){" +
-                        "if(item){" +
-                        "test = 1;}" +
-                        "});");
-                write("if(test != 0 ){" +
-                        "return false;}" +
-                        "return true;");
-                break;
-            case "match any":
-                write("var array-test = [");
-                for (Metadata mtd : children) {
-                    write(mtd.readable());
-                    if (children.indexOf(mtd) != children.size() - 1) {
-                        write(", ");
-                    }
-                }
-                write("];");
-                write("array-test.forEach(function(item){" +
-                        "if(item){" +
-                        "return true;}" +
-                        "});");
-                break;
-            case "sum":
-                write("array-test.reduce(function(acc,item)" +
-                        "{ return acc + item;" +
-                        "});");
 
-                break;
-
-            case "min":
-                write("Math.min(array-test);");
-                break;
-            default:
-                write("Nary-Error");// will create a syntax error in js
-                // if narymetadata operator unrecognized
-                break;
-        }
-        write("}\n");
     }
 
     @Override
-    public void endMetadata(NaryMetadata metadata, int depth) {
-        try {
-            write("\n");
-            ops.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public void startMetadata(StepWhen metadata, int depth){
+        write("function when(){if(");
     }
 
     @Override
-    public void visitMetadata(ValidationRule metadata, int depth) {
-    }
-
-    @Override
-    public void visitMetadata(StepWhen metadata, int depth) {
-
-        write("if(");
-        visit(metadata.stepCondition().getMetadata(), depth + 1);
-        write("){ return true; }\n");
-    }
-
-    @Override
-    public void visitMetadata(StepCondition metadata, int depth) {
+    public void endMetadata(StepWhen metadata,int depth){
+        write("){return true;}return false; }\n");
     }
 
     protected void write(String str) {
