9b80e57d11aecc5a990bff01d4c89ed85fbc7b83
jru <justin.rudat@lesfurets.com>
2018-10-30 15:03:22 +0100

    adding engine evaluation of moment.min.js
    adding mock data for the purpose of testing
    
    adding new data, WIP problem to fix on parenthesis
    
    fixing the parenthesis problem, now using moment.diff(date,unit) instead of moment.diff(date)

--- sample/validation/src/test/java/io/doov/sample/validation/ast/RulesVisitorTest.java
+++ sample/validation/src/test/java/io/doov/sample/validation/ast/RulesVisitorTest.java
@@ -15,8 +15,7 @@ package io.doov.sample.validation.ast;
 import static io.doov.core.dsl.impl.DefaultRuleRegistry.REGISTRY_DEFAULT;
 import static io.doov.core.dsl.meta.i18n.ResourceBundleProvider.BUNDLE;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.util.Locale;
 
@@ -39,8 +38,8 @@ public class RulesVisitorTest {
     public void print_full_syntax_tree() {
         StringBuilder sb = new StringBuilder();
         REGISTRY_DEFAULT.stream()
-                        .peek(rule -> sb.append("--------------------------------").append("\n"))
-                        .forEach(rule -> rule.accept(new AstFullVisitor(sb), 0));
+                .peek(rule -> sb.append("--------------------------------").append("\n"))
+                .forEach(rule -> rule.accept(new AstFullVisitor(sb), 0));
         System.out.println(sb.toString());
     }
 
@@ -48,8 +47,8 @@ public class RulesVisitorTest {
     public void print_line_syntax_tree() {
         StringBuilder sb = new StringBuilder();
         REGISTRY_DEFAULT.stream()
-                        .peek(rule -> sb.append("--------------------------------").append("\n"))
-                        .forEach(rule -> rule.accept(new AstLineVisitor(sb, BUNDLE, Locale.ENGLISH), 0));
+                .peek(rule -> sb.append("--------------------------------").append("\n"))
+                .forEach(rule -> rule.accept(new AstLineVisitor(sb, BUNDLE, Locale.ENGLISH), 0));
         System.out.println(sb.toString());
     }
 
@@ -57,8 +56,8 @@ public class RulesVisitorTest {
     public void print_text_syntax_tree() {
         StringBuilder sb = new StringBuilder();
         REGISTRY_DEFAULT.stream()
-                        .peek(rule -> sb.append("--------------------------------").append("\n"))
-                        .forEach(rule -> rule.accept(new AstTextVisitor(sb, BUNDLE, Locale.ENGLISH), 0));
+                .peek(rule -> sb.append("--------------------------------").append("\n"))
+                .forEach(rule -> rule.accept(new AstTextVisitor(sb, BUNDLE, Locale.ENGLISH), 0));
         System.out.println(sb.toString());
     }
 
@@ -66,8 +65,8 @@ public class RulesVisitorTest {
     public void print_markdown_syntax_tree() {
         StringBuilder sb = new StringBuilder();
         REGISTRY_DEFAULT.stream()
-                        .peek(rule -> sb.append("--------------------------------").append("\n"))
-                        .forEach(rule -> rule.accept(new AstMarkdownVisitor(sb, BUNDLE, Locale.ENGLISH), 0));
+                .peek(rule -> sb.append("--------------------------------").append("\n"))
+                .forEach(rule -> rule.accept(new AstMarkdownVisitor(sb, BUNDLE, Locale.ENGLISH), 0));
         System.out.println(sb.toString());
     }
 
@@ -75,14 +74,14 @@ public class RulesVisitorTest {
     public void print_html_syntax_tree() {
         ByteArrayOutputStream ops = new ByteArrayOutputStream();
         REGISTRY_DEFAULT.stream()
-                        .peek(rule -> {
-                            try {
-                                ops.write("--------------------------------\n".getBytes());
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        })
-                        .forEach(rule -> rule.accept(new AstHtmlVisitor(ops, BUNDLE, Locale.ENGLISH), 0));
+                .peek(rule -> {
+                    try {
+                        ops.write("--------------------------------\n".getBytes());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                })
+                .forEach(rule -> rule.accept(new AstHtmlVisitor(ops, BUNDLE, Locale.ENGLISH), 0));
         System.out.println(new String(ops.toByteArray(), Charset.forName("UTF-8")));
     }
 
@@ -105,27 +104,29 @@ public class RulesVisitorTest {
         //TODO test the engine evaluation, solve the import moment js problem
 //        //WIP - need to creation javascript object to instanciate the account/configuration values to real values
 
-
         ScriptEngineManager sem = new ScriptEngineManager();
         ScriptEngine engine = sem.getEngineByName("nashorn");
-
         REGISTRY_DEFAULT.stream().forEach(rule -> {
             ops.reset();
             try {
-//                ops.write("import ~/node_modules/moment/moment.js;\n".getBytes());
-                ops.write("var account = {email:\"potato@tomato.com\"};".getBytes("UTF-8"));
+                ops.write("var configuration = { max:{email:{size:\"24\"}}, min:{age:\"18\"}};".getBytes("UTF-8"));
+                ops.write("var account = {email:\"potato@tomato.com\", creation: {date : \"10-10-2000\"}, country:\"FR\", phone:{number:\"+33555555555\"}};".getBytes("UTF-8"));
+                ops.write("var user = {birthdate:\"09-08-1993\",first:{name:\"french\"}, last:{name:\"fries\"} };".getBytes("UTF-8"));
+
+                rule.accept(new AstProceduralJSVisitor(ops, BUNDLE, Locale.ENGLISH), 0);
+                String request = new String(ops.toByteArray(), Charset.forName("UTF-8"));
+                try {
+                    InputStreamReader reader = new InputStreamReader(new FileInputStream(new File("/home/jru/moment.min.js")));
+                    engine.eval(reader);
+                    System.out.println("    Starting engine checking of : " + rule.readable());
+                    Object obj = engine.eval(request);
+                    System.out.println(obj.toString());
+                    System.out.println("    Ending engine checking.");
+                } catch (final ScriptException se) {
+                    throw new RuntimeException(se);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-            rule.accept(new AstProceduralJSVisitor(ops, BUNDLE, Locale.ENGLISH), 0);
-            String request = new String (ops.toByteArray(), Charset.forName("UTF-8"));
-            try{
-                System.out.println("    Starting engine checking of : "+rule.readable());
-                Object obj = engine.eval(request);
-                System.out.println(obj.toString());
-                System.out.println("    Ending engine checking.");
-            }catch(final ScriptException se){
-                throw new RuntimeException(se);
             }
         });
 
