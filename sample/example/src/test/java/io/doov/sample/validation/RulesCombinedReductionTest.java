86ff8e27d7b4caf8bcb16c1b3fe80f063ab1403d
gdu <gdu@lesfurets.com>
2018-11-26 15:28:17 +0100

    improved failure cause tests - fixed a locale issue

--- sample/example/src/test/java/io/doov/sample/validation/RulesCombinedReductionTest.java
+++ sample/example/src/test/java/io/doov/sample/validation/RulesCombinedReductionTest.java
@@ -4,6 +4,8 @@ import static io.doov.assertions.Assertions.assertThat;
 import static io.doov.core.dsl.DOOV.matchAll;
 import static io.doov.core.dsl.DOOV.matchNone;
 import static io.doov.core.dsl.DOOV.not;
+import static io.doov.core.dsl.lang.ReduceType.FAILURE;
+import static io.doov.core.dsl.lang.ReduceType.SUCCESS;
 import static io.doov.sample.field.dsl.DslSampleModel.accountCompany;
 import static io.doov.sample.field.dsl.DslSampleModel.accountCountry;
 import static io.doov.sample.field.dsl.DslSampleModel.when;
@@ -16,6 +18,7 @@ import static io.doov.sample.model.Company.OODRIVE;
 import static io.doov.sample.model.Country.UK;
 import static io.doov.sample.model.Country.US;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import io.doov.core.dsl.DOOV;
@@ -25,101 +28,115 @@ import io.doov.sample.model.SampleModel;
 import io.doov.sample.model.SampleModels;
 
 public class RulesCombinedReductionTest {
-    SampleModel sample = SampleModels.sample();
+    private SampleModelRule rule;
+    private final SampleModel sample = SampleModels.sample();
+    private Result result;
 
     @Test
     public void test_account() {
-        SampleModelRule demoRule = when(matchNone(
-                        accountCompany.anyMatch(DAILYMOTION, BLABLACAR),
-                        accountCountry.anyMatch(UK, US))).validate();
-        Result result = demoRule.withShortCircuit(false).executeOn(sample);
+        rule = when(matchNone(
+                accountCompany.anyMatch(DAILYMOTION, BLABLACAR),
+                accountCountry.anyMatch(UK, US))).validate();
+
+        result = rule.withShortCircuit(false).executeOn(sample);
         assertThat(result).isTrue();
     }
 
     @Test
     public void test_account_KO() {
+        rule = when(matchNone(
+                accountCompany.anyMatch(DAILYMOTION, BLABLACAR),
+                accountCountry.anyMatch(UK, US))).validate();
+
         sample.getAccount().setCompany(DAILYMOTION);
-        SampleModelRule demoRule = when(matchNone(
-                        accountCompany.anyMatch(DAILYMOTION, BLABLACAR),
-                        accountCountry.anyMatch(UK, US))).validate();
-        Result result = demoRule.withShortCircuit(false).executeOn(sample);
-        System.out.println(result.getFailureCause());
+        result = rule.withShortCircuit(false).executeOn(sample);
         assertThat(result).isFalse();
     }
 
     @Test
     public void test_account_2() {
-        SampleModelRule demoRule = when(matchNone(
-                        accountCompany.eq(DAILYMOTION).or(accountCompany.eq(BLABLACAR)),
-                        accountCountry.eq(UK).or(accountCountry.eq(US)))).validate();
-        Result result = demoRule.withShortCircuit(false).executeOn(sample);
+        rule = when(matchNone(
+                accountCompany.eq(DAILYMOTION).or(accountCompany.eq(BLABLACAR)),
+                accountCountry.eq(UK).or(accountCountry.eq(US)))).validate();
+
+        result = rule.withShortCircuit(false).executeOn(sample);
         assertThat(result).isTrue();
     }
 
     @Test
     public void test_account_2_KO() {
+        rule = when(matchNone(
+                accountCompany.eq(DAILYMOTION).or(accountCompany.eq(BLABLACAR)),
+                accountCountry.eq(UK).or(accountCountry.eq(US)))).validate();
+
         sample.getAccount().setCompany(DAILYMOTION);
-        SampleModelRule demoRule = when(matchNone(
-                        accountCompany.eq(DAILYMOTION).or(accountCompany.eq(BLABLACAR)),
-                        accountCountry.eq(UK).or(accountCountry.eq(US)))).validate();
-        Result result = demoRule.withShortCircuit(false).executeOn(sample);
-        System.out.println(result.getFailureCause());
+        result = rule.withShortCircuit(false).executeOn(sample);
         assertThat(result).isFalse();
     }
 
     @Test
     public void test_account_3() {
-        SampleModelRule demoRule = when(not(accountCompany.eq(DAILYMOTION).or(accountCompany.eq(BLABLACAR))
-                        .or(accountCountry.eq(UK).or(accountCountry.eq(US))))).validate();
-        Result result = demoRule.withShortCircuit(false).executeOn(sample);
+        rule = when(not(accountCompany.eq(DAILYMOTION).or(accountCompany.eq(BLABLACAR))
+                .or(accountCountry.eq(UK).or(accountCountry.eq(US))))).validate();
+
+        result = rule.withShortCircuit(false).executeOn(sample);
         assertThat(result).isTrue();
     }
 
     @Test
     public void test_account_3_KO() {
         sample.getAccount().setCompany(DAILYMOTION);
-        SampleModelRule demoRule = when(not(accountCompany.eq(DAILYMOTION).or(accountCompany.eq(BLABLACAR))
-                        .or(accountCountry.eq(UK).or(accountCountry.eq(US))))).validate();
-        Result result = demoRule.withShortCircuit(false).executeOn(sample);
-        System.out.println(result.getFailureCause());
+        rule = when(not(accountCompany.eq(DAILYMOTION).or(accountCompany.eq(BLABLACAR))
+                .or(accountCountry.eq(UK).or(accountCountry.eq(US))))).validate();
+
+        result = rule.withShortCircuit(false).executeOn(sample);
         assertThat(result).isFalse();
     }
 
     @Test
     public void test_account_4() {
-        SampleModelRule demoRule = when(matchAll(accountCompany.anyMatch(LES_FURETS, CANAL_PLUS, MEETIC, OODRIVE),
-                        accountCountry.anyMatch(UK, US).not())).validate();
-        Result result = demoRule.withShortCircuit(false).executeOn(sample);
+        rule = when(matchAll(accountCompany.anyMatch(LES_FURETS, CANAL_PLUS, MEETIC, OODRIVE),
+                accountCountry.anyMatch(UK, US).not())).validate();
+
+        result = rule.withShortCircuit(false).executeOn(sample);
         assertThat(result).isTrue();
     }
 
     @Test
     public void test_account_4_KO() {
+        rule = when(matchAll(accountCompany.anyMatch(LES_FURETS, CANAL_PLUS, MEETIC, OODRIVE),
+                accountCountry.anyMatch(UK, US).not())).validate();
+
         sample.getAccount().setCompany(DAILYMOTION);
-        SampleModelRule demoRule = when(matchAll(accountCompany.anyMatch(LES_FURETS, CANAL_PLUS, MEETIC, OODRIVE),
-                        accountCountry.anyMatch(UK, US).not())).validate();
-        Result result = demoRule.withShortCircuit(false).executeOn(sample);
-        System.out.println(result.getFailureCause());
+        result = rule.withShortCircuit(false).executeOn(sample);
         assertThat(result).isFalse();
     }
 
     @Test
     public void test_account_5() {
-        SampleModelRule demoRule = when(matchAll(accountCompany.eq(LES_FURETS).or(accountCompany.eq(CANAL_PLUS)
-                        .or(accountCompany.eq(MEETIC).or(accountCompany.eq(OODRIVE)))),
-                        not(accountCountry.eq(UK).or(accountCountry.eq(US))))).validate();
-        Result result = demoRule.withShortCircuit(false).executeOn(sample);
+        rule = when(matchAll(accountCompany.eq(LES_FURETS).or(accountCompany.eq(CANAL_PLUS)
+                .or(accountCompany.eq(MEETIC).or(accountCompany.eq(OODRIVE)))),
+                not(accountCountry.eq(UK).or(accountCountry.eq(US))))).validate();
+
+        result = rule.withShortCircuit(false).executeOn(sample);
         assertThat(result).isTrue();
     }
 
     @Test
     public void test_account_5_KO() {
+        rule = when(matchAll(accountCompany.eq(LES_FURETS).or(accountCompany.eq(CANAL_PLUS)
+                .or(accountCompany.eq(MEETIC).or(accountCompany.eq(OODRIVE)))),
+                DOOV.not(accountCountry.eq(UK).or(accountCountry.eq(US))))).validate();
+
         sample.getAccount().setCompany(DAILYMOTION);
-        SampleModelRule demoRule = when(matchAll(accountCompany.eq(LES_FURETS).or(accountCompany.eq(CANAL_PLUS)
-                        .or(accountCompany.eq(MEETIC).or(accountCompany.eq(OODRIVE)))),
-                        DOOV.not(accountCountry.eq(UK).or(accountCountry.eq(US))))).validate();
-        Result result = demoRule.withShortCircuit(false).executeOn(sample);
-        System.out.println(result.getFailureCause());
+        result = rule.withShortCircuit(false).executeOn(sample);
         assertThat(result).isFalse();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println(rule + " is " + result.value());
+        System.out.println("SUCCESS> " + result.reduce(SUCCESS));
+        System.out.println("FAILURE> " + result.reduce(FAILURE));
     }
 }
