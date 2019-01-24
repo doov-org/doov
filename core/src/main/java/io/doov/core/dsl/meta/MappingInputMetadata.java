2745b12cf2085a134265d04b1052aa457573b36a
gdu <gdu@lesfurets.com>
2019-01-25 00:29:57 +0100

    license headers

--- core/src/main/java/io/doov/core/dsl/meta/MappingInputMetadata.java
+++ core/src/main/java/io/doov/core/dsl/meta/MappingInputMetadata.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Courtanet
+ * Copyright 2017 Courtanet
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
