3374d120bf229df5be5cd6bdc71b47008e6a4668
ogu <Ozan.Gunalp@lesfurets.com>
2019-09-13 15:06:25 +0200

    Empty live code class

---
+++ sample/example/src/test/java/io/doov/sample/RulesCodeOne2019Test.java
@@ -0,0 +1,18 @@
+package io.doov.sample;
+
+import io.doov.sample.model.SampleModel;
+import io.doov.sample.model.SampleModels;
+import io.doov.sample.validation.RulesOld;
+
+/**
+ * @see RulesOld
+ */
+public class RulesCodeOne2019Test {
+    SampleModel sample = SampleModels.sample();
+    /**
+     * @see RulesOld#validateAccount(io.doov.sample.model.User, io.doov.sample.model.Account,
+     *      io.doov.sample.model.Configuration)
+     */
+
+
+}
