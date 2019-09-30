8b202f5b756989b262b19f0f6e8a6c8e5cbbf060
ogu <Ozan.Gunalp@lesfurets.com>
2019-09-30 10:51:09 +0200

    Demo CodeOne2019

--- sample/example/src/test/java/io/doov/sample/RulesCodeOne2019Test.java
+++ sample/example/src/test/java/io/doov/sample/RulesCodeOne2019Test.java
@@ -1,7 +1,15 @@
 package io.doov.sample;
 
-import io.doov.sample.model.SampleModel;
-import io.doov.sample.model.SampleModels;
+import static io.doov.core.dsl.DOOV.matchAll;
+import static io.doov.core.dsl.time.LocalDateSuppliers.today;
+import static io.doov.sample.field.dsl.DslSampleModel.*;
+
+import org.junit.jupiter.api.Test;
+
+import io.doov.assertions.Assertions;
+import io.doov.core.dsl.lang.Result;
+import io.doov.sample.field.dsl.DslSampleModel.*;
+import io.doov.sample.model.*;
 import io.doov.sample.validation.RulesOld;
 
 /**
@@ -13,6 +21,19 @@ public class RulesCodeOne2019Test {
      * @see RulesOld#validateAccount(io.doov.sample.model.User, io.doov.sample.model.Account,
      *      io.doov.sample.model.Configuration)
      */
+    @Test
+    void validate_email() {
+        SampleModelRule rule = when(matchAll(
+                userBirthdate.ageAt(today()).greaterOrEquals(18),
+                accountEmail.length().lesserOrEquals(configurationMaxEmailSize),
+                accountCompany.eq(Company.LES_FURETS),
+                accountPhoneNumber.startsWith("+33")
+        )).validate();
+        sample.getAccount().setCompany(Company.CANAL_PLUS);
+        Result result = rule.executeOn(sample);
 
+        System.out.println(rule.markdown());
 
+        Assertions.assertThat(result).isTrue();
+    }
 }
